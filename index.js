9b42973ba38a28a30877ea9a169f65641920c010
AyyItsAljo <aljaz.medic@gmail.com>
2020-07-31 20:42:33 +0200

    Added quick greet command

--- index.js
+++ index.js
@@ -14,19 +14,24 @@ const bot = new Bot("?");
 
 bot.onReady(() => {
   console.info(`Logged in as ${bot.user.tag}!`);
-  console.info(`Url invite: ${bot.createInvite()}`)
+  console.info(`Url invite: ${bot.createInvite()}`);
 });
 
-bot.use(randomChance(0.5), (msg, client, params, next) => {
-  console.log("RANDOMMM");
-  next();
-});
+bot.register(
+  "greet",
+  randomChance(0.5),
+  (msg, client, params, next) => {
+    console.log("RANDOMMM");
+    next();
+  },
+  (msg, client, params) => {
+    console.log("PARAMS", client);
+    msg.reply("Hi!");
+  }
+);
 
-bot.register("greet", (msg, client, params) => {
-  console.log("PARAMS", client);
-  msg.reply("Hi!");
-});
+bot.registerDirectory("./commands"
+, { skip: true });
 
-bot.registerDirectory("./commands");
-
+console.log(bot.commands)
 bot.start(DISCORD_TOKEN);
