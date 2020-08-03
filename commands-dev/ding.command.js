7401c3704b2c958d93ab50aaa639ebb0e810e38f
AyyItsAljo <aljaz.medic@gmail.com>
2020-08-03 12:30:57 +0200

    for commands in development

---
+++ commands-dev/ding.command.js
@@ -0,0 +1,11 @@
+
+export default {
+    name: 'ding',
+    aliases:['dong', 'ching'],
+	before: [],
+	description:"www.racistanswer.com",
+	run: function(msg, client, params) {
+		//final function
+		msg.reply('long dong ching chong');
+	},
+};
