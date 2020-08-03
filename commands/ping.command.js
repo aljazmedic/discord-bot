92e50f5799b99e63901ac9a87b70448415b82dad
AyyItsAljo <aljaz.medic@gmail.com>
2020-08-03 12:31:32 +0200

    Rewrite for es6 parsing

--- commands/ping.command.js
+++ commands/ping.command.js
@@ -1,13 +1,13 @@
-const Ping = (msg, client, params) => {
-    //final function
-    console.log(params);
-    msg.reply('pong');
+export default {
+	name: "ping", //name of the command
+
+	before: [], // middleware functions
+
+	aliases: ["testp"],
+
+	run: (msg, client, params) => {
+		//final function
+		console.log(params);
+		msg.reply("pong");
+	},
 };
-
-Ping.name = 'ping'; //name of the command
-
-Ping.before = []; // middleware functions
-
-Ping.aliases = ['testp'];
-
-export default Ping;
