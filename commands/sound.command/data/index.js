46b810403293bb1fb2b8359a737f3ee886dbd35d
AyyItsAljo <aljaz.medic@gmail.com>
2020-08-16 21:31:15 +0200

    new sounds

--- commands/sound.command/data/index.js
+++ commands/sound.command/data/index.js
@@ -1,9 +1,32 @@
-import fs from 'fs'
+const all = {};
 
-const soundFiles = {};
+const meme = {
+	mission: '1Mission Failed.0013', //https://www.memesoundboard.com
+	tbc: '1To%20Be%20Continued.0038',
+	gay: '1Im%20G*y.0072',
+	nokia: '1Nokia Ringtone.0546',
+	why:'1Why.0065',
+	nice:'1Noice.0084'
+};
 
-fs.readdir(__dirname, (err, files) => {
-    files.forEach((file)=>{
-        
-    });
+const yt = {
+	fart: 'W_FRPoJIrlI',
+	pog: 'FZUcpVmEHuk',
+	elbow: 'pr_kkWVnHoo',
+};
+
+Object.entries(meme).forEach(([k, v]) => {
+	if (Object.keys(all).includes(k)) {
+		console.error(`${k} already defined as sound!`);
+	}
+	all[k] = { q: v, src: 'meme' };
 });
+
+Object.entries(yt).forEach(([k, v]) => {
+	if (Object.keys(all).includes(k)) {
+		console.error(`${k} already defined as sound!`);
+	}
+	all[k] = { q: v, src: 'yt' };
+});
+
+export default all;
