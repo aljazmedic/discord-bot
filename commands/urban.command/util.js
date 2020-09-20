b3fc9210508e512a89811db782dbf1e1b8a0bbcc
AyyItsAljo <aljaz.medic@gmail.com>
2020-09-21 00:03:03 +0200

    conditional definition and examples

--- commands/urban.command/util.js
+++ commands/urban.command/util.js
@@ -1,16 +1,20 @@
 import { MessageEmbed } from 'discord.js';
 
 export function fromDataToEmbed(d) {
-    console.log(d)
-	let { definition="", word, permalink, author, example="" } = d;
-    definition = definition.replace(/[[\]]+/gi, '');
-    example = example.replace(/[[\]]+/gi, '');
-    console.log(definition)
-	return new MessageEmbed()
+	console.log(d);
+	let { definition = '', word, permalink, author, example = '' } = d;
+	definition = definition.replace(/[[\]]+/gi, '');
+	example = example.replace(/[[\]]+/gi, '');
+	console.log(definition, example);
+	const me = new MessageEmbed()
 		.setTitle(word.toUpperCase())
 		.setURL(permalink)
 		.setAuthor(author)
-		.addField('Definition', definition)
-        .addField('Example', example)
-        .setFooter("UrbanDict via RapidApi", "https://rapidapi.com/static-assets/default/favicon.ico");
+		.setFooter(
+			'UrbanDict via RapidApi',
+			'https://rapidapi.com/static-assets/default/favicon.ico',
+		);
+	if (definition) me.addField('Definition', definition);
+	if (example) me.addField('Example', example);
+	return me;
 }
